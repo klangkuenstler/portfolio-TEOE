5839caeeb1919d001c30b98af5d450bbfe7d624f
Marcos Ribeiro <143022166+klangkuenstler@users.noreply.github.com>
2025-12-29 03:59:12 +0000

    Unfunctional commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,7 +50,7 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 }
 
 // add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+// modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
 
 
